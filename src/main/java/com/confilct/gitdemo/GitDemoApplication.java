7881aca5437262856da728d03c3d9995b65f1142
zgqbubbling <13061167655@163.com>
2023-12-26 15:02:18 +0800

    master second commit

--- src/main/java/com/confilct/gitdemo/GitDemoApplication.java
+++ src/main/java/com/confilct/gitdemo/GitDemoApplication.java
@@ -9,6 +9,7 @@ public class GitDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitDemoApplication.class, args);
         System.out.println("test one");
+        System.out.println("master test one");
     }
 
 }
