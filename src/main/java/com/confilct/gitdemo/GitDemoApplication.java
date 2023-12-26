b9a0401bc762ff62ace7e21609f1a98b3d0298cb
zgqbubbling <13061167655@163.com>
2023-12-26 14:49:24 +0800

    Revert "secondBranch first commit"
    This reverts commit 08aaa3ce069f752f724e758d03880a3313e15896.

--- src/main/java/com/confilct/gitdemo/GitDemoApplication.java
+++ src/main/java/com/confilct/gitdemo/GitDemoApplication.java
@@ -8,7 +8,6 @@ public class GitDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitDemoApplication.class, args);
-        SpringApplication.run(GitDemoApplication.class, args);
     }
 
 }
